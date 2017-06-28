7ae0fc055ea27d8a609633704ed68f5586cd132d
Balmung421 <zcook421@gmail.com>
2017-06-27 21:54:15 -0600

    this is zach's git test

---
+++ lib/credit_check.rb
@@ -0,0 +1,21 @@
+#due thursday
+
+card_number = "4929735477250543"
+
+valid = false
+
+# Your Luhn Algorithm Here
+#luhn part 1
+def luhn_algorithm
+puts "I am working!"
+end
+#luhn double everyother number
+#luhn add +1 over 10
+#sum all the numbers
+
+
+
+# Output
+luhn_algorithm
+## If it is valid, print "The number is valid!"
+## If it is invalid, print "The number is invalid!"
